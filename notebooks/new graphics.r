91aff7c66547704eb89470fb5ed2f90d3d570575
jonbright <jonathan.bright@oii.ox.ac.uk>
2024-01-31 11:44:56 +0000

    Update new graphics.r

--- notebooks/new graphics.r
+++ notebooks/new graphics.r
@@ -3,5 +3,29 @@ library(ggplot2)
 
 data_dir <- 'C:/Users/simon/Desktop/Turing/Projects/decision-services-index/data/processed/'
 
-
 df <- read_xlsx(paste0(data_dir,'202308-services-list-processed.xlsx'))
+
+table(df$organisation)
+
+table(df$service_type, useNA = 'always')
+
+sum(table(df$service_type, useNA = 'always'))
+
+
+
+#Number of services currently on offer
+##by type
+##by department
+##digital sign on required or not
+##network diagram - shows overlap of types. good potential for services to be shared across departments 
+##parallel / correlation between web forms 
+
+#Volumes
+##Historic transaction volumes
+##Distribution of transaction volumes (Zipf)
+##Partial mapping to contemporary services 
+##Parallel / correlation between web visits 
+
+#Automation 
+##Share of routine tasks overall (Fig3b)
+##breakdown by task type (Fig3A)
